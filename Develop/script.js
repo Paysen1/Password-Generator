7c73d3105d9c00b991e8afe000a39b8cf13477c2
Peak <pglund18@yahoo.com>
2023-05-17 22:42:59 -0500

    Changing/ length assignment and random gene fixup

--- Develop/script.js
+++ Develop/script.js
@@ -1,48 +1,56 @@
 // Assignment Code
-var passwordLength = length
-var special=["!", "#" , "$" , "%" , "&" , "'" , "(" , ")" , "*" , "+" , "," , "-" , "." , "/" , ":" , ";" , "<" , "=" , ">" , "?" , "@" , "[" , "]" , "^" , "_" , "`" , "{" , "|" , "}" , "~" , ];
+var special=["!", "#" , "$" , "%" , "&" , "'" , "(" , ")" , "*" , "+" , "," , "-" , "." , "/" , ":" , ";" , "<" , "=" , ">" , "?" , "@" , "[" , "]" , "^" , "_" , "`" , "{" , "|" , "}" , "~" ];
 var letters=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numbers=["1","2","3","4","5","6","7","8","9"];
 var lowers=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-function start(){
-var length = window.prompt("How long would you like your password?")
-  console.log(length)
+
+var length = window.prompt("How long would you like your password?/ 8-128")
+  console.log(length);
+
 var specialC = window.confirm("Would you like special characters?")
 if (specialC === true){
-  console.log(specialC)
-  console.log(special)
+  console.log(special);
 }
 var caps = window.confirm("Would you like uppercase lettering?")
 if (caps === true){
-  console.log(caps)
-  console.log(letters)
+  console.log(letters);
 }
 var lower = window.confirm("Would you like lowercase lettering?")
 if (lower === true){
-  console.log(lower)
-  console.log(lowers)
+  console.log(lowers);
 }
 var num = window.confirm ("would you like numbers in your password?")
 if (num === true){
-  console.log(num)
-  console.log(numbers)
+  console.log(numbers);
 }
 
-}
-start()
+const random = [
+  function getRandomLetter() {
+    return letters[Math.floor(Math.random() * letters.length)];
+  },
+  function getRandomLower() {
+    return lowers[Math.floor(Math.random() * lowers.length)];
+  },
+  function getRandomNumber() {
+    return numbers[Math.floor(Math.random() * numbers.length)];
+  },
+  function getRandomSpecial() {
+    return special[Math.floor(Math.random() * special.length)];
+  }
+];
+
+
+function generatePassword(){
+  for (let index = 0; index < passwordLength.length; index++) {
+    const element = random[index];
+    element()
+  }
+};
+  
 
 
 var generateBtn = document.querySelector("#generate");
-
-function generatePassword(){
-  for (var i = 0; i < passwordLength; i++) {
-    var randomNumber = special[Math.floor(special.length * math.random())];
-    document.write(randomNumber)
-   }
-
-  
-}
 
 // Write password to the #password input
 function writePassword() {
@@ -52,7 +60,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
-writePassword();
+
 
 
 // Add event listener to generate button
